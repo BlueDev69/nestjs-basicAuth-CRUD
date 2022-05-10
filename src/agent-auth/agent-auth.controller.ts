28a8c980a07356a0de170fffd5caa0cee4357a2b
Kittanat-V <kittanat@Kittanats-MacBook-Pro.local>
2022-05-10 17:20:58 +0700

    forgot save file

--- src/agent-auth/agent-auth.controller.ts
+++ src/agent-auth/agent-auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiGatewayTimeoutResponse,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/user.model';
+import { AgentAuthService } from './agent-auth.service';
 
 @Controller('agent-auth')
-export class AgentAuthController {}
+export class AgentAuthController {
+  constructor(private agentAuthService: AgentAuthService) {}
+
+  @Post('/login')
+  @ApiOkResponse({ description: 'User Login' })
+  @ApiUnauthorizedResponse({ description: 'Invalid credentials' })
+  @ApiBody({ type: LoginBody })
+  async login(
+    @Body('user', ValidationPipe) credentials: LoginDTO,
+  ): Promise<ResponseObject<'user', AuthResponse>> {
+    const user = await this.agentAuthService.login(credentials);
+    return { user };
+  }
+}
